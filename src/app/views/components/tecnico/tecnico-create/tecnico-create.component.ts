7715c81eb4c08cf3faeca6f579362cb93c0e5fe2
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2021-10-11 16:11:21 -0300

    adiciona post para um tecnico

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Tecnico } from "src/app/models/tecnico";
+import { TecnicoService } from "src/app/services/tecnico.service";
 
 @Component({
-  selector: 'app-tecnico-create',
-  templateUrl: './tecnico-create.component.html',
-  styleUrls: ['./tecnico-create.component.css']
+  selector: "app-tecnico-create",
+  templateUrl: "./tecnico-create.component.html",
+  styleUrls: ["./tecnico-create.component.css"],
 })
 export class TecnicoCreateComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: "",
+    nome: "Peter",
+    cpf: "856.768.050-64",
+    telefone: "(00) 90000-0000",
+  };
 
-  constructor(private router : Router) { }
+  constructor(private router: Router, private service: TecnicoService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  cancel(): void {
+    this.router.navigate(["tecnicos"]);
   }
 
-  cancel():void{
-    this.router.navigate(['tecnicos'])
+  create(): void {
+    this.service.create(this.tecnico).subscribe(
+      (resposta) => {
+        this.router.navigate(["tecnicos"]);
+        this.service.message("Técnico criado com sucesso!");
+      },
+      (err) => {
+        if (err.error.error.match("já cadastrado")) {
+          this.service.message(err.error.error);
+        }
+      }
+    );
   }
 }
