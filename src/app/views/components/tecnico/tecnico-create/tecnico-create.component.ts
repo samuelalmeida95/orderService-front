b26bf36735f84b982abb9d0d09bf49f444a78529
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2021-10-16 12:46:45 -0300

    Adicona  validações para criação de um técnico

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Tecnico } from "src/app/models/tecnico";
 import { TecnicoService } from "src/app/services/tecnico.service";
@@ -11,10 +12,14 @@ import { TecnicoService } from "src/app/services/tecnico.service";
 export class TecnicoCreateComponent implements OnInit {
   tecnico: Tecnico = {
     id: "",
-    nome: "Peter",
-    cpf: "856.768.050-64",
-    telefone: "(00) 90000-0000",
+    nome: "",
+    cpf: "",
+    telefone: "",
   };
+
+  nome = new FormControl('', [Validators.minLength(5)])
+  cpf = new FormControl('', [Validators.minLength(11)])
+  telefone = new FormControl('', [Validators.minLength(11)])
 
   constructor(private router: Router, private service: TecnicoService) {}
 
@@ -33,8 +38,20 @@ export class TecnicoCreateComponent implements OnInit {
       (err) => {
         if (err.error.error.match("já cadastrado")) {
           this.service.message(err.error.error);
+        } else if (
+          err.error.erros[0].message ===
+          "número do registro de contribuinte individual brasileiro (CPF) inválido"
+        ) {
+          this.service.message("CPF inválido!");
         }
       }
     );
+  }
+
+  erroNomeInvalido(){
+    if(this.nome.invalid){
+      return 'O nome deve ter entre 5 e 100 caracteres'
+    }
+    return false;
   }
 }
