229d1f1af16f307fd57ede4260ed96283b09d21c
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2021-11-06 16:30:20 -0300

    Modifica a listagem de tecnicos e clientes para que traga o nome no lugar do id

--- src/app/views/components/os/os-read/os-read.component.ts
+++ src/app/views/components/os/os-read/os-read.component.ts
@@ -1,43 +1,76 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { Router } from '@angular/router';
-import { OS } from 'src/app/models/os';
-import { OsService } from 'src/app/services/os.service';
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { Router } from "@angular/router";
+import { OS } from "src/app/models/os";
+import { ClienteService } from "src/app/services/cliente.service";
+import { OsService } from "src/app/services/os.service";
+import { TecnicoService } from "src/app/services/tecnico.service";
 
 @Component({
-  selector: 'app-os-read',
-  templateUrl: './os-read.component.html',
-  styleUrls: ['./os-read.component.css']
+  selector: "app-os-read",
+  templateUrl: "./os-read.component.html",
+  styleUrls: ["./os-read.component.css"],
 })
 export class OsReadComponent implements AfterViewInit {
+  lista: OS[] = [];
 
-  lista: OS [] = [];
-
-  displayedColumns: string[] = ['tecnico', 'cliente', 'abertura', 'fechamento', 'prioridade', 'status', 'actions'];
+  displayedColumns: string[] = [
+    "tecnico",
+    "cliente",
+    "abertura",
+    "fechamento",
+    "prioridade",
+    "status",
+    "actions",
+  ];
   dataSource = new MatTableDataSource<OS>(this.lista);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
-    private service : OsService,
-    private router : Router){}
+    private service: OsService,
+    private router: Router,
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService
+  ) {}
 
   ngAfterViewInit() {
     this.findAll();
   }
 
-  findAll():void {
-   const subscription =  this.service.findAll().subscribe((resposta) => {
+  findAll(): void {
+    const subscription = this.service.findAll().subscribe((resposta) => {
       this.lista = resposta;
+      this.listarTecnico();
+      this.listarCliente();
       this.dataSource = new MatTableDataSource<OS>(this.lista);
       this.dataSource.paginator = this.paginator;
       subscription.unsubscribe();
-    })
+    });
   }
 
-  navigateToCreate():void {
-    this.router.navigate(['os/create'])
+  navigateToCreate(): void {
+    this.router.navigate(["os/create"]);
   }
 
+  listarTecnico(): void {
+    this.lista.forEach((ordemServico) => {
+      this.tecnicoService
+        .findById(ordemServico.tecnico)
+        .subscribe((resposta) => {
+          ordemServico.tecnico = resposta.nome;
+        });
+    });
+  }
+
+  listarCliente(): void {
+    this.lista.forEach((ordemServico) => {
+      this.clienteService
+        .findById(ordemServico.cliente)
+        .subscribe((resposta) => {
+          ordemServico.cliente = resposta.nome;
+        });
+    });
+  }
 }
