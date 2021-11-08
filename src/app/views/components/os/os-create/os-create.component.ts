7f8cf1ab6b088e58ab16701ad23cc376fccb64bd
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2021-11-08 10:41:13 -0300

    Adiciona componente UPDATE para ordem de serviço

--- src/app/views/components/os/os-create/os-create.component.ts
+++ src/app/views/components/os/os-create/os-create.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { Cliente } from "src/app/models/cliente";
+import { OS } from "src/app/models/os";
 import { Tecnico } from "src/app/models/tecnico";
 import { ClienteService } from "src/app/services/cliente.service";
+import { OsService } from "src/app/services/os.service";
 import { TecnicoService } from "src/app/services/tecnico.service";
 
 @Component({
@@ -10,19 +13,40 @@ import { TecnicoService } from "src/app/services/tecnico.service";
   styleUrls: ["./os-create.component.css"],
 })
 export class OsCreateComponent implements OnInit {
-  selected = "";
+
+  os: OS = {
+    tecnico: "",
+    cliente: "",
+    observacoes: "",
+    status: "",
+    prioridade: "",
+  };
 
   tecnicos: Tecnico[] = [];
   clientes: Cliente[] = [];
 
   constructor(
     private tecnicoService: TecnicoService,
-    private clienteService: ClienteService
+    private clienteService: ClienteService,
+    private service: OsService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.listarTecnicos();
     this.listarClientes();
+  }
+
+  create(): void {
+    console.log(this.os)
+    this.service.create(this.os).subscribe((resposta) => {
+      this.service.message("Ordem de serviço criada com sucesso!");
+      this.router.navigate(["os"]);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['os'])
   }
 
   listarTecnicos(): void {
